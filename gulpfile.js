c1001538d1b436f2f2ebf023377719f0647b1448
Curtis Jurgensen <curtisj44@hotmail.com>
2015-10-15 20:19:38 -0700

    adding autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp'),
+
+	autoprefixer = require('gulp-autoprefixer'),
 	sass = require('gulp-sass');
 
 gulp.task('sass', function () {
 	gulp.src('./src/styles/**/*.sass')
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 5 versions'],
+			cascade: false
+		}))
 		.pipe(gulp.dest('./dist/styles'));
 });
 
