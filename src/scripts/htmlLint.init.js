f4e2bb1cf9cf81dc6a3379dc35c32164ec131562
Curtis Jurgensen <curtisj44@hotmail.com>
2015-10-15 23:48:55 -0700

    special handling for the mock pages

--- src/scripts/htmlLint.init.js
+++ src/scripts/htmlLint.init.js
@@ -2,38 +2,42 @@
 	'use strict';
 
 	htmlLint.init = function () {
-		if (typeof $ === 'undefined') {
-			$ = jQuery;
-		}
-
-		htmlLint.editFlash();
-
-		var output = '<div id="html-lint" style="display:none">' +
+		var $htmlLint = $('#html-lint'),
+			isMock = window.location.protocol === 'file:' && window.location.pathname.indexOf('/mocks/') > 0,
+			output = '<div id="html-lint" style="display:none">' +
 				'<h1>HTML-Lint</h1>' +
 				'<h2>Total Errors</h2>' +
 				'<button class="html-lint-button html-lint-close">&times;</button>' +
 				'<ol class="html-lint-tab-list"></ol>' +
 				'</div>';
 
-		if ($('#html-lint').length > 0) {
-			$('#html-lint').fadeOut(250, function () {
+		console.log(isMock);
+
+		if (typeof $ === 'undefined') $ = jQuery;
+
+		if (!isMock) {
+			htmlLint.editFlash();
+
+			if ($htmlLint.length > 0) {
+				$htmlLint.fadeOut(250, function () {
+					// TODO - make DRYer
+					$('body').append(output);
+				});
+			} else {
 				// TODO - make DRYer
 				$('body').append(output);
-			});
-		} else {
-			// TODO - make DRYer
-			$('body').append(output);
-		}
+			}
 
-		$.each(htmlLint.panel, function (index, value) {
-			htmlLint.panel[index]();
-		});
+			$.each(htmlLint.panel, function (index, value) {
+				htmlLint.panel[index]();
+			});
+
+			$htmlLint.fadeIn(250, function () {
+				$(this).removeAttr('style');
+			});
+		}
 
 		htmlLint.close();
 		htmlLint.tabSetup();
-
-		$('#html-lint').fadeIn(250, function () {
-			$(this).removeAttr('style');
-		});
 	};
 }(window.htmlLint = window.htmlLint || {}));
