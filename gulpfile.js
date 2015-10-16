45002c75288e150477c950e3243043148f11ae64
Curtis Jurgensen <curtisj44@hotmail.com>
2015-10-15 22:45:07 -0700

    setting up JS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
 
 	autoprefixer = require('gulp-autoprefixer'),
+	concat = require('gulp-concat'),
 	sass = require('gulp-sass');
+	// uglify = require('gulp-sass');
 
-gulp.task('sass', function () {
+gulp.task('styles', function () {
 	gulp.src('./src/styles/**/*.sass')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
@@ -13,4 +15,17 @@ gulp.task('sass', function () {
 		.pipe(gulp.dest('./dist/styles'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('scripts', function () {
+	gulp.src('./src/scripts/**/*.js')
+		.pipe(concat('html-lint.js'))
+		// .pipe(gulp.dest('./dist/scripts/'))
+		// .pipe(uglify())
+		// .pipe(uglify({ mangle: { toplevel: true } })
+		// 	.on('error', gulpUtil.log))
+		// .pipe(rename({
+		// 	suffix: '.min'
+		// }))
+		.pipe(gulp.dest('./dist/scripts/'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
