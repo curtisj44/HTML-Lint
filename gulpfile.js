79ebb505f9ea35e731fe4ae3fb8628590dffa56a
Curtis Jurgensen <curtisj44@hotmail.com>
2015-12-17 23:30:23 -0800

    restructuring to run the clean, compile, and minify tasks synchonously

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,71 @@
 var gulp = require('gulp'),
 
+	// dependencies
 	autoprefixer = require('gulp-autoprefixer'),
 	concat = require('gulp-concat'),
 	del = require('del'),
 	minifier = require('gulp-minifier'),
+	notify = require('gulp-notify'),
 	rename = require('gulp-rename'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
 
-gulp.task('clean', function () {
-	// del.sync('./dist/**');
+	// paths
+	paths = {
+		scripts: './src/scripts/**/*.js',
+		styles: './src/styles/**/*.sass'
+	};
+
+gulp.task('default', ['scripts', 'styles']);
+
+gulp.task('clean-styles', function () {
+	return del(['./dist/*.min.css']);
 });
 
-gulp.task('minify', function () {
+gulp.task('clean-scripts', function () {
+	return del(['./dist/*.min.js']);
+});
+
+gulp.task('scripts', ['clean-scripts'], function () {
 	gulp
-		.src('./dist/*')
+		.src(paths.scripts)
+		.pipe(concat('htmlLint.js'))
+		.pipe(gulp.dest('./dist'))
+		.pipe(notify('Compiled scripts: [<%= file.relative %>]'))
 		.pipe(minifier({
 			minify: true,
 			collapseWhitespace: true,
 			conservativeCollapse: true,
 			minifyJS: true,
-			minifyCSS: true
+			minifyCSS: false
 		}))
 		.pipe(rename({
 			suffix: '.min'
 		}))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(notify('Minified scripts: [<%= file.relative %>]'));
 });
 
-gulp.task('scripts', function () {
+gulp.task('styles', ['clean-styles'], function () {
 	gulp
-		.src('./src/scripts/**/*.js')
-		.pipe(concat('htmlLint.js'))
-		.pipe(gulp.dest('./dist'));
-});
-
-gulp.task('styles', function () {
-	gulp
-		.src('./src/styles/**/*.sass')
+		.src(paths.styles)
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 5 versions'],
 			cascade: false
 		}))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./dist'))
+		.pipe(notify('Compiled styles: [<%= file.relative %>]'))
+		.pipe(minifier({
+			minify: true,
+			collapseWhitespace: true,
+			conservativeCollapse: true,
+			minifyJS: false,
+			minifyCSS: true
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(gulp.dest('./dist'))
+		.pipe(notify('Minified styles: [<%= file.relative %>]'));
 });
 
-gulp.task('default', ['clean', 'scripts', 'styles', 'minify']);
